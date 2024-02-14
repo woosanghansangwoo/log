3f024f1c7cc777f8a3e473741baafb004d29dcb4
root <root@blog.clouda.cloud>
2024-02-14 11:13:06 +0900

    add utterances

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "woosanghansangwoo/log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
